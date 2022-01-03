593f9faeacea5fa6cda75ed62d044bc2e7d56bee
FlautistaMacabro <vinicius.pelegrineli@unesp.br>
2022-01-03 17:29:40 -0300

    Botão da Área de Testes finalizado

--- js/toggleTestArea.js
+++ js/toggleTestArea.js
@@ -1,15 +1,15 @@
 function toggleTestArea() {
-    let btt = document.getElementById("bttopentestarea");
-    let divContent = document.getElementById("divcontenttestarea");
     if(typeof(toggleTestArea.ativo) == 'undefined')
         toggleTestArea.ativo = false;
     if(!toggleTestArea.ativo){
         toggleTestArea.ativo = true;
         document.getElementById("divtestareatable").style.display = "block";
+        document.getElementById("divtestareabtt").style.display = "grid";
     }else {
         toggleTestArea.ativo = false;
         document.getElementById("divtestareatable").style.display = "none";
+        document.getElementById("divtestareabtt").style.display = "none";
     }
-    btt.classList.toggle("content");
-    divContent.classList.toggle("display"); 
+    document.getElementById("bttopentestarea").classList.toggle("content");
+    document.getElementById("divcontenttestarea").classList.toggle("display"); 
 }

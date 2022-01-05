38f98e2feea6b14a39696532228be28a868baf64
FlautistaMacabro <vinicius.pelegrineli@unesp.br>
2022-01-05 13:37:40 -0300

    Menu simples para edição do AF e Adição de estados simples no AF

---
+++ js/afVisualization/navFunctions.js
@@ -0,0 +1,52 @@
+var quantNodes = 0;
+
+function selectBtt(btt) {
+    cy.removeAllListeners();
+}
+
+function editBtt(btt) {
+    cy.removeAllListeners();
+}
+
+function addNodeBtt(btt) {
+    cy.removeAllListeners();
+    cy.on('tap', function(event){
+        var evtTarget = event.target;
+        
+        if(evtTarget === cy){
+            cy.add({
+                group: 'nodes',
+                data: { id: `q${quantNodes}`, weight: 75 },
+                renderedPosition: {
+                    x: event.renderedPosition.x,
+                    y: event.renderedPosition.y,
+                }
+            });
+            quantNodes++;
+        }
+    });
+}
+
+function addArrowBtt(btt) {
+    cy.removeAllListeners();
+}
+
+function removeBtt(btt) {
+    cy.removeAllListeners();
+}
+
+
+window.addEventListener('load', function init() {
+    let btts = document.querySelectorAll(".cynavbtt");
+    for (let i = 0; i < btts.length; i++)
+        if(btts[i].id == 'selectbtt')
+            btts[i].addEventListener('click', (event) => selectBtt(event));
+        else if(btts[i].id == 'editbtt')
+            btts[i].addEventListener('click', (event) => editBtt(event));
+        else if(btts[i].id == 'addnodebtt')
+            btts[i].addEventListener('click', (event) => addNodeBtt(event));
+        else if(btts[i].id == 'addarrowbtt')
+            btts[i].addEventListener('click', (event) => addArrowBtt(event));
+        else if(btts[i].id == 'removebtt')
+            btts[i].addEventListener('click', (event) => removeBtt(event));
+})

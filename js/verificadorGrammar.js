14bd8a4f3a9a19ea86da61ea54cdfaa734d7ad63
marble-zone <gp.araujo@unesp.br>
2022-01-23 16:55:58 -0300

    Mais desenvolvimento na gramatica

--- js/verificadorGrammar.js
+++ js/verificadorGrammar.js
@@ -2,35 +2,40 @@ function verificaSentencaGramatica(rules, sentence, start = 'S'){
   let curRules = new Set();
   curRules.add(start);
   let regrasSeguintes = new Set();
+  regrasSeguintes.clear();
   //primeiro loop, verifica ate que a sentenca esteja vazia
-  while(sentence != ""){
+  while(sentence !== ""){
     for(const linhaAtual of curRules.values()){
-      for(const regraAtual of rules[linhaAtual]){
-        if(regraAtual === "\\"){;}
-        //1th caractere da sentenca eh igual ao 1th da regra
-        else if(sentence[0] === regraAtual[0]){
-          if(regraAtual[1] != "")
-            regrasSeguintes.add(regraAtual[1]);
-          else
-            regrasSeguintes.add("\\");
-        }
-        //caso seja uma regra sem literal
-        else if(regraAtual != "" && regraAtual === regraAtual.toUpperCase()){
-          regrasSeguintes.add(regraAtual[0]);
+      if(linhaAtual !== "\\"){
+        for(const regraAtual of rules[linhaAtual]){
+          if(regraAtual === "\\"){;}
+          //1th caractere da sentenca eh igual ao 1th da regra
+          else if(sentence[0] === regraAtual[0]){
+            if(regraAtual.length >= 2)
+              regrasSeguintes.add(regraAtual[1]);
+            else
+              regrasSeguintes.add("\\");
+          }
+          //caso seja uma regra sem literal
+          else if(regraAtual !== "" && regraAtual === regraAtual.toUpperCase()){
+            regrasSeguintes.add(regraAtual[0]);
+          }
         }
       }
     }
 
     //configuracao para proxima iteracao do while(externo)
-    if(!regrasSeguintes.has(NaN) && !regrasSeguintes.has(undefined)){
-      curRules = regrasSeguintes;
-      regrasSeguintes = new Set();
-      sentence = sentence.slice(1);
-    }else if(sentence.length > 1) 
-      return(false);
+    
+    sentence = sentence.slice(1);
+    if((regrasSeguintes.size <= 0) && (sentence.length > 0)) return false;
+    else if(sentence.length > 0){
+      curRules = new Set(regrasSeguintes);
+      regrasSeguintes.clear();
+    }else break;
   }
 
-  regrasSeguintes = new Set();
+  curRules = new Set(regrasSeguintes);
+  regrasSeguintes.clear();
   const maxTentativas = 100;
   let contTentativa = 0;
   //checagem do restante das regras apos o fim da sentenca
@@ -40,7 +45,7 @@ function verificaSentencaGramatica(rules, sentence, start = 'S'){
       else{
         for(const regraAtual of rules[linhaAtual]){
           if(regraAtual === "\\") return(true);
-          else if(regraAtual != "" && regraAtual === regraAtual.toUpperCase()){
+          else if(regraAtual !== "" && regraAtual === regraAtual.toUpperCase()){
             regrasSeguintes.add(regraAtual);
           }
         }
@@ -49,8 +54,8 @@ function verificaSentencaGramatica(rules, sentence, start = 'S'){
 
     //configuracao para proxima iteracao do while(externo)
     contTentativa++;
-    curRules = regrasSeguintes;
-    regrasSeguintes = new Set();
+    curRules = new Set(regrasSeguintes);
+    regrasSeguintes.clear();
   }
 
   //nada mais a tentar, portanto falhou.
